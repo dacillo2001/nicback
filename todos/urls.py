4bee369ca26896b0bb19c59f66da462cbb299684
dacillo2001 <dacillonicole8@gmail.com>
2025-04-22 23:57:02 +0800

    c

--- todos/urls.py
+++ todos/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet)  # This registers the '/tasks/' endpoint
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),  # This should include all the URLs from the router
+    path('', include(router.urls)),
 ]
